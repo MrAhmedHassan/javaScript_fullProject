8575154c5433fbe44bb8fed0452983da95534f1d
abdalrahaman-adel <abdalrahmanadel380@gmail.com>
2019-12-24 19:09:03 +0200

    added js file

---
+++ js/contact-us.js
@@ -0,0 +1,7 @@
+let name = document.querySelector(".input1");
+let email = document.querySelector(".input2");
+let subject = document.querySelector(".input3"); 
+let message = document.querySelector(".msg");
+
+
+
