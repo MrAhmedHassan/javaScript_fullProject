8ea7d363eb5e04933d94d721cced40ccf5a4cbe0
abdalrahaman-adel <abdalrahmanadel380@gmail.com>
2019-12-29 23:48:29 +0200

    image,catergory,product name and description were added

--- js/showItem.js
+++ js/showItem.js
@@ -1,4 +1,4 @@
-console.log("Enjoy Mr Abdo :)");
+// console.log("Enjoy Mr Abdo :)");
 // let url = window.location.href;
 // let alllQueryString = url.split("?")[1];
 // console.log(queryString);
@@ -6,7 +6,7 @@ console.log("Enjoy Mr Abdo :)");
 // console.log(subQueryString);
 let subQueryString = [
   "productId=HT-1002",
-  "imgLink=https://openui5.hana.ondemand.com/test-resources/sap/ui/documentation/sdk/images/HT-1002.jpg",
+  "imgLink = https://openui5.hana.ondemand.com/test-resources/sap/ui/documentation/sdk/images/HT-1002.jpg",
   "category=Laptops",
   "productName=Notebook%20Basic%2018",
   "description=Notebook%20Basic%2018%20with%202,80%20…M,%201000%20GB%20Hard%20Disc,%20Windows%208%20Pro",
@@ -14,4 +14,36 @@ let subQueryString = [
   "productQuantity=10"
 ];
 
-console.log(subQueryString);
+// console.log(subQueryString);
+
+// let productImage = subQueryString.splice("=");
+// let image =(productImage[1]);
+// console.log(image);
+// let imageHolder = document.getElementById("img1");
+// imageHolder.setAttribute("src",image)
+// console.log(imageHolder);
+
+//  to insert an image
+let image = subQueryString[1].split("=")[1];
+let imageHolder = document.getElementById("img1");
+imageHolder.setAttribute("src", image);
+
+// to insert category and description
+let cat = subQueryString[2].split("=")[1];
+let catHolder = document.getElementById("cat1");
+catHolder.innerHTML=cat;
+
+
+// to insert product name
+let pro = subQueryString[3].split("=")[1].split("%20");
+let proHolder = document.getElementById("proname");
+proHolder.innerHTML=pro;
+
+// to insert description
+let desc = subQueryString[4].split("=")[1].split("%20");
+let descHolder = document.getElementById("desc");
+descHolder.innerHTML=desc;
+
+
+
+
