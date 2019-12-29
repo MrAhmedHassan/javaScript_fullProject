1b13f2990d3c31cd458ca53c0dd52b8ddaadb44c
MrAhmedHassan <MrAhmedHassan95@gmail.com>
2019-12-29 16:04:21 +0200

    create js and css and html page for this feature

---
+++ js/showItem.js
@@ -0,0 +1,17 @@
+console.log("Enjoy Mr Abdo :)");
+// let url = window.location.href;
+// let alllQueryString = url.split("?")[1];
+// console.log(queryString);
+// subQueryString = queryString.split("&");
+// console.log(subQueryString);
+let subQueryString = [
+  "productId=HT-1002",
+  "imgLink=https://openui5.hana.ondemand.com/test-resources/sap/ui/documentation/sdk/images/HT-1002.jpg",
+  "category=Laptops",
+  "productName=Notebook%20Basic%2018",
+  "description=Notebook%20Basic%2018%20with%202,80%20…M,%201000%20GB%20Hard%20Disc,%20Windows%208%20Pro",
+  "productPrice=1570",
+  "productQuantity=10"
+];
+
+console.log(subQueryString);
