ab5f3e8e358eec4ba91a9cdfe8dfe138bbe730e6
MrAhmedHassan <MrAhmedHassan95@gmail.com>
2019-12-31 19:13:09 +0200

    improve in showItem page style

--- js/showItem.js
+++ js/showItem.js
@@ -9,7 +9,7 @@ let subQueryString = [
   "imgLink = https://openui5.hana.ondemand.com/test-resources/sap/ui/documentation/sdk/images/HT-1002.jpg",
   "category=Laptops",
   "productName=Notebook%20Basic%2018",
-  "description=Notebook%20Basic%2018%20with%202,80%20…M,%201000%20GB%20Hard%20Disc,%20Windows%208%20Pro",
+  'description=Notebook%20Basic%2017%20with%202,80%20GHz%20quad%20core,%2017"%20LCD,%204%20GB%20DDR3%20RAM,%20500%20GB%20Hard%20Disc,%20Windows%208%20Pro ',
   "productPrice=1570",
   "productQuantity=10"
 ];
@@ -36,7 +36,7 @@ catHolder.innerHTML = cat;
 // to insert product name
 let pro = subQueryString[3]
   .split("=")[1]
-  .split("%")
+  .split("%20")
   .join(" ");
 let proHolder = document.getElementById("proname");
 proHolder.innerHTML = pro;
@@ -44,15 +44,42 @@ proHolder.innerHTML = pro;
 // to insert description
 let desc = subQueryString[4]
   .split("=")[1]
-  .split("%")
+  .split("%20")
   .join(" ");
 console.log(desc);
-let productDesc = desc.replace("%", " ");
-console.log(productDesc);
 let descHolder = document.getElementById("desc");
 descHolder.innerHTML = desc;
 
 // to insert price
-let pri = subQueryString[5].split("=")[1];
-let priHolder = document.getElementById("1");
-priHolder.innerHTML = pri + "$";
+let price = subQueryString[5].split("=")[1];
+let priceDiv = document.getElementById("price");
+priceDiv.value = price;
+priceDiv.innerHTML = price + "$";
+// priHolder.innerHTML = pri + "$";
+
+let maxQuantity = +subQueryString[6].split("=")[1];
+console.log(maxQuantity);
+
+let productId = subQueryString[0].split("=")[1];
+console.log(productId);
+
+let selectBox = document.querySelector("#selectBox");
+for (let i = 1; i <= maxQuantity; i++) {
+  let optionInSelect = document.createElement("option");
+  optionInSelect.value = i;
+  optionInSelect.innerHTML = i;
+  selectBox.appendChild(optionInSelect);
+  // console.log(object);
+}
+
+if (localStorage.getItem("sideCart")) {
+  localStorageData = JSON.parse(localStorage.getItem("sideCart"));
+  console.log(localStorageData);
+  localStorageData.forEach(element => {
+    if (element.Id == productId) {
+      element.number;
+    }
+  });
+}
+
+let AddToCart = document.querySelector("#add");
